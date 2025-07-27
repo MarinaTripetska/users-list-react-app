23973a1ba04769b90c9976f6f7c7d7fffbbbe0b7
Marina Tripetska <marina.tripetskaya@gmail.com>
2025-07-27 17:40:45 +0200

    feat: base structure for UserPage component

--- src/pages/UsersPage.tsx
+++ src/pages/UsersPage.tsx
@@ -1,10 +1,71 @@
+import { useEffect, useState } from 'react';
+import {useNavigate} from "react-router";
+import UsersApiService from "@/services/UsersApiService.ts";
+import type { User } from "@/types/UsersApiResponse.ts";
+
 function UsersPage() {
+    const api = UsersApiService.create();
+    const navigate = useNavigate();
+
+
+    const [users, setUsers] = useState<User[]>([]);
+    const [loading, setLoading] = useState<boolean>(false);
+    const [error, setError] = useState<string | null>(null);
+    const [search, setSearch] = useState<string>("");
+
+    useEffect(() => {
+        const fetchUsers = async () =>{
+            setLoading(true);
+            try {
+                const users = await api.getAllUsers();
+                setUsers(users);
+            } catch(e) {
+                if(e instanceof Error){
+                    setError(e.message + "Failed to load users.")
+                }
+            }
+        }
+
+        fetchUsers().finally(() => setLoading(false));
+    }, []);
+
+
+
+
     return (
-        <>
-            <h1>
-               Users page
-            </h1>
-        </>
+        <main>
+            <div>
+                <h1>Users page</h1>
+
+                <input
+                    type="text"
+                    placeholder="Search users..."
+                    value={search}
+                    onChange={(e) => setSearch(e.target.value)}
+                />
+
+                <div>
+                    {loading && <p>Loading...</p>}
+
+                    {error && <p>{error}</p>}
+
+                    <div>
+                        {users.map((user) => (
+                            <div key={user.id}>
+                                <h3>
+                                    {user.firstName} {user.lastName}
+                                </h3>
+
+                                <button onClick={() => navigate(`/users/${user.id}`)}>
+                                    Details
+                                </button>
+                            </div>
+                        ))}
+                    </div>
+
+                </div>
+            </div>
+        </main>
     )
 }
 
