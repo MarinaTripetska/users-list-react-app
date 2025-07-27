02b224292e4b373437ee62b5d78286a3b0678bdf
Marina Tripetska <marina.tripetskaya@gmail.com>
2025-07-27 17:39:28 +0200

    feat: add alias for module imports

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,6 @@
 import {defineConfig, loadEnv} from 'vite'
+import {fileURLToPath, URL} from 'node:url'
+
 import react from '@vitejs/plugin-react'
 
 export default defineConfig(({mode}) => {
@@ -8,6 +10,12 @@ export default defineConfig(({mode}) => {
     return {
         plugins: [react()],
         base: env.VITE_BASE_URL,
+        resolve: {
+            alias: {
+                '@': fileURLToPath(new URL('./src', import.meta.url))
+            }
+        },
+
         server: {
             port: 3000,
         }
