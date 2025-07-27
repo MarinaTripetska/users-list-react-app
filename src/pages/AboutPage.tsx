8129009a71c8d61fc4c1acf697e59b0c3a2ddd52
Marina Tripetska <marina.tripetskaya@gmail.com>
2025-07-27 23:54:36 +0200

    feat: add lorem on About page

--- src/pages/AboutPage.tsx
+++ src/pages/AboutPage.tsx
@@ -1,10 +1,29 @@
 function AboutPage() {
     return (
-        <>
-            <h1>
-               About page
-            </h1>
-        </>
+        <main>
+            <div>
+                <h1>
+                    About page
+                </h1>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi cum distinctio maxime minus officia
+                    quaerat quam similique suscipit. Aperiam culpa cum debitis, iure nesciunt qui rem totam. Ad
+                    assumenda dicta ea laborum nemo obcaecati, pariatur praesentium quidem rerum voluptatum! Amet atque
+                    debitis delectus et possimus quam repudiandae, saepe sequi veniam!
+                </p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab animi asperiores atque debitis delectus
+                    dolor earum enim esse, hic id laboriosam, laborum mollitia odio officia quos repellat saepe, sint
+                    soluta tempora temporibus tenetur ut voluptates? Aut, iure, laborum laudantium maxime nihil nobis
+                    nulla perspiciatis porro praesentium quia quisquam, quod ratione suscipit! Ad adipisci aperiam
+                    asperiores autem, cum cumque doloremque eius eligendi excepturi illo incidunt ipsam iste magni odit
+                    optio provident quas quia, quo recusandae, reiciendis reprehenderit vel voluptas voluptate! Aliquid
+                    aspernatur blanditiis consequatur delectus earum eius est explicabo in, inventore laborum molestiae,
+                    molestias neque nobis omnis praesentium quidem, repellat rerum.</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet, aperiam aspernatur dolores error nemo
+                    numquam quo sunt ut? A ab aliquid aspernatur corporis dicta dolor eaque, earum eligendi fugit id
+                    iure molestiae nihil odio officia perferendis porro quae, quam quas quis repellat sit tempora
+                    tempore vero voluptate voluptatibus. Dolor, minus.</p>
+            </div>
+        </main>
     )
 }
 
